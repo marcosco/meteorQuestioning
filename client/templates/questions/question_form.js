03f606863dfe699522663532327f13c9d0f38329
Marco Scordino <marco.scordino@bsfactory.net>
2015-01-12 18:22:45 +0100

    refactoring question insert methos

--- client/templates/questions/question_form.js
+++ client/templates/questions/question_form.js
@@ -6,7 +6,12 @@ Template.questionForm.events({
       title: $(e.target).find('[name=questionTitle]').val(),
       text: $(e.target).find('[name=questionText]').val()
     }
-    Meteor.call("addQuestion", question);
+    Meteor.call("addQuestion", question, function(error, id) {
+      if (error)
+        return alert(error.reason);
+
+      Router.go('questionPage', {_id: id});
+    });
 
     // Clear form
     e.target.questionText.value = "";
