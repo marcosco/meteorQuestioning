bf6380c9b04bf10db6278e8af36271934b5b4098
Marco Scordino <marco.scordino@bsfactory.net>
2015-04-09 11:19:44 +0200

    license info

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,3 +1,23 @@
+/*
+Questioning is a kind of BB system completely written using MeteorJS that
+implements Ann Kerwin's Ignorance Map and a rewarding system to involve users.
+Copyright (c) 2015, Author: Marco Scordino, Advisor: Paolo Ceravolo http://www.di.unimi.it/ceravolo/
+
+This file is part of Questioning.
+
+Questioning is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Questioning is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Questioning.  If not, see <http://www.gnu.org/licenses/>.
+*/
 Meteor.startup(function () {
 
   function findOrCreateUser(name, admin) {
@@ -155,7 +175,7 @@ Meteor.startup(function () {
       loadSeeds("seeds/css-" + seed + ".json");
     }
 
-    classifier.addDocument("Marco Scordino", "daddy"); 
+    classifier.addDocument("My pet", "Pitt"); 
     classifier.train();
     classifier.save('assets/app/classifier.json', function(err, classifier) {
       // the classifier is saved to the classifier.json file!
